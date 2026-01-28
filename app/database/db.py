fad7c524bb153e8f0e3d1889b202229108bc3deb
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-28 17:47:38 +0100

    fix: correct word spelling

--- app/database/db.py
+++ app/database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine('sqlite:///banksystem.db', connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
