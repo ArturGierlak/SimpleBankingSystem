d57c3d6b56129ac562191a86df87d7fd3269ef9e
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-28 14:54:15 +0100

    feat: create pydantic schema for transactions

---
+++ app/schemas/transaction.py
@@ -0,0 +1,20 @@
+import datetime
+from decimal import Decimal
+from pydantic import BaseModel
+
+
+class TransactionBase(BaseModel):
+    client_id: int
+    transaction_type: str
+    amount: Decimal
+
+class TransactionCreate(TransactionBase):
+    pass
+
+class TransactionResponse(TransactionBase):
+    id: int
+    timestamp: datetime
+    balance_after: Decimal
+
+    class Config():
+        orm_more = True
