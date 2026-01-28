bc5199d8283d4c4b729675c37e1345ce78be0a7e
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-28 16:45:53 +0100

    feat: add function listing transactions for the client

--- app/repositories/transactions.py
+++ app/repositories/transactions.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from sqlalchemy import select, delete
 from sqlalchemy.orm import Session
 from app.models.transaction import Transaction
-
+from app.models.client import Client
 
 class TransactionRepository:
     def __init__(self, db: Session):
@@ -16,3 +16,8 @@ class TransactionRepository:
         self.db.commit()
         self.db.refresh(transaction)
         return transaction
+    
+    def list(self, client_id: int):
+        statement = select(Transaction).where(Client.id == client_id)
+        return list(self.db.execute(statement)._allrows())
+    
