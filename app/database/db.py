80521745a8bde774445c1aa86120cc50fc824d06
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-28 17:34:10 +0100

    fix: correct session and db handling

--- app/database/db.py
+++ app/database/db.py
@@ -3,5 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine('sqlite:///banksystem.db', connect_args={"check_same_thread": False})
 
-Session = sessionmaker(bind=engine)
-session = Session()
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
