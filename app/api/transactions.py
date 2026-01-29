89a02ce2df4753fb626364c9ee73fa9466e4860c
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-29 11:46:24 +0100

    feat: add endpoints listing client's transactions and all transactions

--- app/api/transactions.py
+++ app/api/transactions.py
@@ -4,6 +4,7 @@ from app.database.db import get_db
 from app.repositories.transactions import TransactionRepository
 from app.schemas.transaction import TransactionResponse, TransactionCreate
 from app.services.transactions import TransactionService
+from app.services.clients import ClientService
 
 router = APIRouter(prefix="/transactions")
 
@@ -13,3 +14,14 @@ def create_transaction(data: TransactionCreate, db: Session = Depends(get_db)):
     return service.create_transaction(client_id=data.client_id,
                                         transaction_type=data.transaction_type,
                                         amount=data.amount)
+
+@router.get("/{client_id}", response_model=list[TransactionResponse])
+def list_client_transactions(client_id: int, db: Session = Depends(get_db)):
+    service_transaction = TransactionService(db)
+    return service_transaction.list_client_transactions(client_id)
+    
+
+@router.get("/", response_model=list[TransactionResponse])
+def list_all_transactions(db: Session = Depends(get_db)):
+    service = TransactionService(db)
+    return service.list_all_transactions()
