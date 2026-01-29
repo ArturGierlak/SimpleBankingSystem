40cfd8a693c44a71c4eb32dd27009bf680e25852
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-29 13:12:16 +0100

    feat: add str and repr function for client

--- app/models/client.py
+++ app/models/client.py
@@ -10,3 +10,9 @@ class Client(Base):
     initial_balance = Column(Numeric(12,2), default=0.0)
 
     transactions = relationship("Transaction", back_populates="client", cascade="all, delete-orphan")
+
+    def __str__(self):
+        return f"Client {self.first_name} {self.last_name} : {self.initial_balance} PLN"
+    
+    def __repr__(self):
+         return f"Client(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, initial_balance={self.initial_balance})"
