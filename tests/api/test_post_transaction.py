635f3a2b80723ce3357c5e9f5198d022e68644ac
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-30 16:32:09 +0100

    feat: test for client's transactions

--- tests/api/test_post_transaction.py
+++ tests/api/test_post_transaction.py
@@ -4,6 +4,7 @@ from app.services.transactions import TransactionService
 from app.schemas.transaction import TransactionResponse
 from datetime import datetime
 from decimal import Decimal
+
 def test_create_transaction(client_with_mocked_db, mocker):
     
     client = client_with_mocked_db
@@ -50,3 +51,33 @@ def test_create_transaction(client_with_mocked_db, mocker):
         transaction_type="deposit",
         amount=100.0
     )
+
+
+def test_list_client_transactions(client_with_mocked_db, mocker):
+    client = client_with_mocked_db
+
+    mocked_list = mocker.patch.object(
+        TransactionService,
+        "list_client_transactions",
+        return_value=[
+            TransactionResponse(
+                id=1,
+                client_id=123,
+                transaction_type="deposit",
+                amount=50.0,
+                timestamp=datetime(2024, 1, 1, 12, 0),
+                balance_after=Decimal("150.0"),
+            )
+        ]
+    )
+
+    response = client.get("/transactions/123")
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert isinstance(data, list)
+    assert data[0]["client_id"] == 123
+    assert float(data[0]["amount"]) == 50.0
+
+    mocked_list.assert_called_once_with(123)
