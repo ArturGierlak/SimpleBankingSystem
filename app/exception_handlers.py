49a17f16f73e9b6daff8280cf56b7e4710f5b19a
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-30 12:09:47 +0100

    feat: add exception for insufficient balance after transactions

--- app/exception_handlers.py
+++ app/exception_handlers.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from app.exceptions import ClientNotFound, ClientAlreadyExists
+from app.exceptions import ClientNotFound, ClientAlreadyExists, InsufficientFundsError
 
 def register_exception_handlers(app: FastAPI):
 
@@ -10,4 +10,8 @@ def register_exception_handlers(app: FastAPI):
     
     @app.exception_handler(ClientAlreadyExists)
     async def client_already_exists_handler(request, exc: ClientAlreadyExists):
+        return JSONResponse(status_code=409, content={"error": str(exc)})
+    
+    @app.exception_handler(InsufficientFundsError)
+    async def insufficient_funds_error_handler(request, exc: InsufficientFundsError):
         return JSONResponse(status_code=409, content={"error": str(exc)})
--- app/repositories/transactions.py
+++ app/repositories/transactions.py
@@ -3,12 +3,20 @@ from sqlalchemy import select, delete
 from sqlalchemy.orm import Session
 from app.models.transaction import Transaction
 from app.models.client import Client
+from app.exceptions import ClientNotFound, InsufficientFundsError
 
 class TransactionRepository:
     def __init__(self, db: Session):
         self.db = db
     
     def create(self, client_id: int, transaction_type: str, amount: Decimal, balance_after: Decimal):
+        statement = select(Client).where(Client.id == client_id)
+        client = self.db.execute(statement).scalars().all()
+        if not client:
+            raise ClientNotFound(f"Client with id={client_id} does not exist.")
+        if balance_after < 0:
+            raise InsufficientFundsError(f"Client's id={client_id} funds are not sufficient.")
+
         transaction = Transaction(client_id = client_id,
                                     transaction_type = transaction_type,
                                     amount = amount,
