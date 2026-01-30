6f74e20af767b33b822ab6552843419c33d9b575
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-30 16:55:59 +0100

    feat: test for transaction repo with insufficient balance exception

--- tests/repositories/test_transaction_repository.py
+++ tests/repositories/test_transaction_repository.py
@@ -3,7 +3,7 @@ from app.repositories.transactions import TransactionRepository
 from app.models.client import Client
 from app.models.enums.transaction_type import TransactionType
 import pytest
-from app.exceptions import ClientNotFound
+from app.exceptions import ClientNotFound, InsufficientFundsError
 
 def test_create_transaction_success(db_session):
     repo = TransactionRepository(db_session)
@@ -36,4 +36,20 @@ def test_create_transaction_client_not_found(db_session):
             transaction_type=TransactionType.DEPOSIT,
             amount=Decimal("50"),
             balance_after=Decimal("150"),
+        )
+
+
+def test_create_transaction_negative_balance(db_session):
+    repo = TransactionRepository(db_session)
+
+    client = Client(id=1, first_name="Test", last_name="Test", initial_balance=Decimal("100"))
+    db_session.add(client)
+    db_session.commit()
+
+    with pytest.raises(InsufficientFundsError):
+        repo.create(
+            client_id=1,
+            transaction_type=TransactionType.WITHDRAWAL,
+            amount=Decimal("200"),
+            balance_after=Decimal("-100"),
         )
