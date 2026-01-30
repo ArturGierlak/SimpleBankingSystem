0c6884cfd9d5c17798a49df6bc4340c589e1b551
Artur Gierlak <artur.gierlak@capgemini.com>
2026-01-30 11:17:14 +0100

    feat: add typical exception classes

---
+++ app/exceptions.py
@@ -0,0 +1,12 @@
+
+class ClientNotFound(Exception):
+    pass
+
+class ClientAlreadyExists(Exception):
+    pass
+
+class NegativeAmountError(Exception):
+    pass
+
+class InsufficientFundsError(Exception):
+    pass
