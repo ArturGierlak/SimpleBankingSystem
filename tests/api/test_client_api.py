e1727e1e42a9c401fdf0296ae0e5b6b50bb54fcb
Artur Gierlak <artur.gierlak@capgemini.com>
2026-02-02 14:33:34 +0100

    test: add endpoint test for creating client

---
+++ tests/api/test_client_api.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from decimal import Decimal
+from app.services.clients import ClientService
+from app.schemas.client import ClientResponse
+
+def test_create_client(client_with_mocked_db, mocker):
+    
+    client = client_with_mocked_db
+
+    
+    mocked_create = mocker.patch.object(
+        ClientService,
+        "create_client",
+        return_value=ClientResponse(
+        id=1,
+        first_name="Test",
+        last_name="Test",
+        initial_balance=Decimal("1000.0"),
+        )
+    )
+
+    payload = {
+        "first_name": "Test",
+        "last_name": "Test",
+        "initial_balance": 1000.0
+    }
+
+    
+    response = client.post("/clients/", json=payload)
+
+    
+    assert response.status_code == 200
+    
+    data = response.json()
+
+    assert data["id"] == 1
+    assert data["first_name"] == "Test"
+    assert data["last_name"] == "Test"
+    assert float(data["initial_balance"]) == 1000.0
+
+    
+    mocked_create.assert_called_once_with(
+        first_name="Test",
+        last_name="Test",
+        initial_balance=1000.0
+    )
